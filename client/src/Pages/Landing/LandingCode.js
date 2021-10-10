59777420bc468c1325300304d278297ac8d1b69a
Nikita Lafinskiy <niklafinskiy@gmail.com>
2021-10-10 15:35:19 +0300

    finished the code section

--- client/src/Pages/Landing/LandingCode.js
+++ client/src/Pages/Landing/LandingCode.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../Styles/Landing/Code.css";
+import "../../Styles/Landing/Landing.css";
 
 export default function LandingProfile() {
   const keyWordsArr = [
@@ -24,25 +24,99 @@ export default function LandingProfile() {
     "</>",
     "</h1>",
     "<h1>",
-    "",
+    "<p>",
+    "</p>",
+    "<span className={obj.color}>",
+    "</span>",
   ];
-  const punctuationMarksArr = ["(", ")", ";", ".", ",", "{", "}", "[", "]"];
+  const punctuationMarksArr = ["(", ")", ";", ".", "{", "}", "[", "]", "()"];
   const numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const methods = ["split", "pop", "map", "includes"];
 
-  const codeText = `import React from "react";import "../../Styles/Landing/Landing.css";export default function LandingProfile() {;
+  const codeText = `import React from "react";
+  import Left from "../../Components/Sides/Left";
+  import Right from "../../Components/Sides/Right";
+  import Sidebar from "../../Components/Sidebar";
+  import LandingProfile from "./LandingProfile";
+  import "../../Styles/Landing/Landing.css";
+  import Main from "../../Components/Main";
+  import LandingCode from "./LandingCode";
+  
+  ;
+  const Landing = () => {;
     return (;
-      <>;
-        <div id='profile'>;
+      <Main>;
+        <Left>;
+        <>;
+        <div>;
           <h1> Nikita Lafinskiy </h1>;
-          <div>;
+          <p>;
             Student of the Kyiv Institute of International Relations. I love;
             coding , and I aspire to become a great developer. I currently code;
             every day developing projects.;
-          </div>;
+          </p>;
         </div>;
       </>;
+        </Left>;
+        <Right>;
+        const codeTextArr = codeText.split(";");
+        codeTextArr. pop ( );
+        //console.log(codeTextArr);
+        const res = codeTextArr. map ((obj) => {;
+          const splitLine = obj. split (" ");
+          //assigning_the_colors_to_each_element_of_the_array;
+          const coloredCode = splitLine. map((obj) => {;
+            if (keyWordsArr. includes (obj)) {;
+              return {;
+                val: obj,;
+                color: "keyword",;
+              };
+            } else if (punctuationMarksArr. includes (obj) ) {;
+              return {;
+                val: obj,;
+                color: "punctuation",;
+              };
+            } else if (numbersArr. includes (obj)) {;
+              return { val: obj, color: "number" };
+            } else if (obj. includes (\`"\`)) {;
+              return {;
+                val: obj,;
+                color: "string",;
+              };
+            } else {;
+              return {;
+                val: obj,;
+                color: "variable",
+              };
+            };
+          });
+      
+          const jsxConverted = coloredCode. map ((obj) => {;
+            //console.log(obj);
+            return <span className={obj.color}> { obj.val + " " } </span>;
+          });
+          return [;
+            <img;
+              style={{;
+                height: "20px",;
+                margin: "0px 5px 0px 5px",;
+                transform: "rotate(90deg)",;
+              }};
+              src='/images/line.png';
+              alt='';
+            />,;
+            ...jsxConverted,;
+            <br/>,;
+          ];
+        });
+        //console.log(res);
+        return <div> { res } </div>;
+        </Right>;
+      </Main>;
     );
-  };`;
+  };
+  
+  export default Landing;`;
   const codeTextArr = codeText.split(";");
   codeTextArr.pop();
   // console.log(codeTextArr);
@@ -60,6 +134,10 @@ export default function LandingProfile() {
           val: obj,
           color: "punctuation",
         };
+      } else if (methods.includes(obj)) {
+        return { val: obj, color: "method" };
+      } else if (obj.includes("//")) {
+        return { val: obj, color: "comment" };
       } else if (numbersArr.includes(obj)) {
         return { val: obj, color: "number" };
       } else if (obj.includes(`"`)) {
@@ -67,6 +145,8 @@ export default function LandingProfile() {
           val: obj,
           color: "string",
         };
+      } else if (obj.includes("<")) {
+        return { val: obj, color: "component" };
       } else {
         return {
           val: obj,
@@ -77,7 +157,7 @@ export default function LandingProfile() {
 
     const jsxConverted = coloredCode.map((obj) => {
       // console.log(obj);
-      return <span className={obj.color}>{obj.val + " "}</span>;
+      return <span className={obj.color}> {obj.val + " "}</span>;
     });
     return [
       <img
@@ -93,6 +173,6 @@ export default function LandingProfile() {
       <br />,
     ];
   });
-  console.log(res);
+  // console.log(res);
   return <div style={{ color: "white" }}>{res}</div>;
 }
